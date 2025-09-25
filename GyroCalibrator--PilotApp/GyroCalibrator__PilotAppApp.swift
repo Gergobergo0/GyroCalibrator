851ef7997ce78e79a8fde3357c1bf723cc5f27cd
Gergobergo0 <157916533+Gergobergo0@users.noreply.github.com>
2025-09-25 14:35:56 +0200

    Create GyroCalibrator__PilotAppApp.swift

---
+++ GyroCalibrator--PilotApp/GyroCalibrator__PilotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GyroCalibrator__PilotAppApp.swift
+//  GyroCalibrator--PilotApp
+//
+//  Created by Gergo Gelegonya on 2025. 09. 05..
+//
+
+import SwiftUI
+
+@main
+struct GyroCalibrator__PilotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
